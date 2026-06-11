9dd189debd19b5fff3f64aefab5d81582dbe73ee
Sakshi09i <sakshisingh9906@gmail.com>
2026-06-11 12:45:36 +0530

    current changes

--- add.js
+++ add.js
@@ -1,10 +1,15 @@
 // sum of two numbers
-function add(a, b) {
-    return a + b;
-}
+// function add(a, b) {
+//     return a + b;
+// }
 
 
-const num1 = 5;
-const num2 = 10;
-const result = add(num1, num2);
-console.log(`The sum of ${num1} and ${num2} is: ${result}`);
+// const num1 = 5;
+// const num2 = 10;
+// const result = add(num1, num2);
+// console.log(`The sum of ${num1} and ${num2} is: ${result}`);
+
+
+const a = 10;
+const b = 20;
+console.log(`The sum of ${a} and ${b} is: ${a + b}`);
